0593a56201cf1660a012a6b3beba36fa4506a94b
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-18 11:55:49 -0400

    adds migrations and models

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+  User.create(name: "marco", email: "marco@marco.net", password_hash: "123")
+  User.first.posts.create(body: "this is a body")
+   User.first.comments.create(body: "this is a comment body")
